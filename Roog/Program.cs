cf6a891906a6e26bcc4d21131fd53cb788b8e189
Tshegofatso <erasmustshegofatso@gmail.com>
2021-05-11 11:25:20 +0200

    i added comment with my name

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -7,6 +7,9 @@ namespace Roog
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //this is a hello world program
+            //tshegofatso mabel erasmus
         }
     }
 }
